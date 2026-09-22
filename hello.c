61ad762c265bd5d17f0de0f0719b29da0b4083cc
ttmkcd <2153364735@qq.com>
2026-09-22 14:06:04 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@ int main(void)
 	int a, b;
     scanf("%d %d" , &a, &b);
     printf("a+b=%d\n", a + b);
-    printf("a+b=%d\n", a * b);
-    
+    printf("a-b=%d\n", a - b);
+    printf("a*b=%d\n", a * b);
     return 0;
 }
