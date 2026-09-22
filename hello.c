3e71977f6646a4b5e83d2ba37b79761f75391178
ttmkcd <2153364735@qq.com>
2026-09-22 08:25:35 +0800

    Update hello.c
    变量版

--- hello.c
+++ hello.c
@@ -2,9 +2,11 @@
 
 int main(void)
 {
-    printf("23+43=%d\n", 23 + 43);
-    printf("87+56=%d\n", 87 + 56);
-    printf("87*56=%d\n", 87 * 56);
+	int a = 23, b=43;
+    printf("23+43=%d\n", a + b);
+    a = 87; b = 56;
+    printf("87+56=%d\n", a + b);
+    printf("87*56=%d\n", a * b);
 
     return 0;
 }
